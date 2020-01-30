3e1944cfbb1fcf44e2da89f8f79083509f1f16c1
Travis Huddleston <travis.huddleston@gmail.com>
2020-01-30 11:08:52 -0500

    To 'B' unit conversion

---
+++ math/ConvertTo-ByteSize.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory)]
+  [ValidatePattern('[0-9\.]+\s?[a-zA-Z]i?[bB]?')]
+  [string]$Size
+)
+
+begin {
+  $unit_map = [ordered]@{
+    'B'  = 0;
+    'KB' = 1;
+    'MB' = 2;
+    'GB' = 3;
+    'TB' = 4;
+    'PB' = 5;
+    'EB' = 6;
+  }
+}
+
+process {
+  $rgx_match = ([regex]'([0-9\.]+)\s?([a-zA-Z]i?[bB]?)').Match($Size)
+  $OrigSize = [double]$rgx_match.Groups[1].Value
+  $Unit = $rgx_match.Groups[2].Value
+
+  if (([char[]]$Unit)[1] -match '[iI]') {
+    $base = 1024
+  }
+  else {
+    $base = 1000
+  }
+  $unit_pow = $unit_map[($unit_map.Keys | Where-Object { ([char[]]$_)[0] -match ([char[]]$Unit)[0] })]
+  return ($OrigSize * [System.Math]::Pow($base, $unit_pow))
+}
